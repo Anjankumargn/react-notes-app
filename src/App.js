62df2370330c4cd065cfd2dfefe79cfc5c746054
anjan <gnanjankumar@gmail.com>
2024-02-29 10:51:10 +0530

    Basic Notes Pages are created.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import { useState } from "react";
+import {nanoid} from "nanoid";
 import NotesList from "./components/NotesList";
 
 const App=()=>{
+     const [notes,setNotes]=useState([
+      {
+      id:nanoid(),  
+      text:"This is my 1st note",
+      date:"29/02/2024"
+     },
+     {
+      id:nanoid(),  
+      text:"This is my 2nd note",
+      date:"27/02/2024"
+     },{
+      id:nanoid(),  
+      text:"This is my 3rd note",
+      date:"21/02/2024"
+     },
+    ])
   return (
      <div className="container">
-      <NotesList/>
+      <NotesList notes={notes}/>
      </div>
   );
 };
